f73b7d379af0c51553fd66c0b3687f8977b87d9e
AbhishekChauhanNITRR <abhishekchauhan5411858@gmail.com>
2025-06-27 00:23:52 +0530

    fix: secure cookie config for cross-origin requests

--- server/Controllers/user.js
+++ server/Controllers/user.js
@@ -87,8 +87,8 @@ exports.login=async(req,res)=>{
 
             res.cookie("token", token, {
                 httpOnly: true,
-                secure: false,
-                sameSite: "Lax",
+                secure: true,
+                sameSite: "None",
                 maxAge: 7*24*60*60*1000 
             })
 
