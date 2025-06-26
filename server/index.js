c224589a8148a7c7562edc4927de70246d2b5d92
AbhishekChauhanNITRR <abhishekchauhan5411858@gmail.com>
2025-06-26 23:41:57 +0530

    fix: removed trailing slash in CORS origin

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const fileUpload = require("express-fileupload");
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: "http://localhost:5173",
+		origin: ["http://localhost:5173","https://campus-meds-nitrr.vercel.app"],
 		credentials: true,
 	})
 );
